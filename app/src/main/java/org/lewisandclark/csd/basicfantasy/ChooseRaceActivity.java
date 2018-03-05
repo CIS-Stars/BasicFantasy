020809d9fcc369e8d8909d2c25bd278210ac061f
Thorin Schmidt <tschmidt@stcharlessd.org>
2018-03-05 15:43:58 -0600

    finalized attribute rollup. added DieRoller class.

---
+++ app/src/main/java/org/lewisandclark/csd/basicfantasy/ChooseRaceActivity.java
@@ -0,0 +1,8 @@
+package org.lewisandclark.csd.basicfantasy;
+
+/**
+ * Created by Thorin Schmidt on 3/5/2018.
+ */
+
+public class ChooseRaceActivity {
+}
--- app/src/main/java/org/lewisandclark/csd/basicfantasy/RollAttributesActivity.java
+++ app/src/main/java/org/lewisandclark/csd/basicfantasy/RollAttributesActivity.java
@@ -1,13 +1,88 @@
 package org.lewisandclark.csd.basicfantasy;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class RollAttributesActivity extends AppCompatActivity {
+
+    public static Intent newIntent(Context packageContext){
+        Intent theIntent = new Intent(packageContext, RollAttributesActivity.class);
+        return theIntent;
+    }
+
+    public static final String TAG = "RollAttributesActivity";
+
+    private Button mRollStats;
+    private Button mAcceptStats;
+    private TextView mStrTextView;
+    private TextView mIntTextView;
+    private TextView mWisTextView;
+    private TextView mDexTextView;
+    private TextView mConTextView;
+    private TextView mChaTextView;
+
+    private int[] mStatArray = {0,0,0,0,0,0};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_roll_attributes);
+
+        mStrTextView = findViewById(R.id.str_score);
+        mStrTextView.setText(Integer.toString(mStatArray[0]));
+
+        mIntTextView = findViewById(R.id.int_score);
+        mIntTextView.setText(Integer.toString(mStatArray[1]));
+
+        mWisTextView = findViewById(R.id.wis_score);
+        mWisTextView.setText(Integer.toString(mStatArray[2]));
+
+        mDexTextView = findViewById(R.id.dex_score);
+        mDexTextView.setText(Integer.toString(mStatArray[3]));
+
+        mConTextView = findViewById(R.id.con_score);
+        mConTextView.setText(Integer.toString(mStatArray[4]));
+
+        mChaTextView = findViewById(R.id.cha_score);
+        mChaTextView.setText(Integer.toString(mStatArray[5]));
+
+        mRollStats = findViewById(R.id.roll_stats);
+        mRollStats.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //generate the random stats
+                Log.d(TAG, "RollStats clicked.");
+                for (int i=0; i<mStatArray.length; i++){
+                    mStatArray[i] = DieRoller.statRoll();
+                }
+
+                mStrTextView.setText(Integer.toString(mStatArray[0]));
+                mIntTextView.setText(Integer.toString(mStatArray[1]));
+                mWisTextView.setText(Integer.toString(mStatArray[2]));
+                mDexTextView.setText(Integer.toString(mStatArray[3]));
+                mConTextView.setText(Integer.toString(mStatArray[4]));
+                mChaTextView.setText(Integer.toString(mStatArray[5]));
+
+            }
+        });
+
+        mAcceptStats = findViewById(R.id.accept_stats);
+        mAcceptStats.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //put stats into player character record.
+                Log.d(TAG, "AcceptStats clicked.");
+            }
+        });
+
+
+
+
     }
 }
