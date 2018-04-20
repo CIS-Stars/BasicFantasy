daa1934fa6e9434875391c48c4ea87fcf2ddb09e
Thorin Schmidt <tschmidt@stcharlessd.org>
2018-04-20 10:17:35 -0500

    small stuff

--- app/src/main/java/org/lewisandclark/csd/basicfantasy/HomeActivity.java
+++ app/src/main/java/org/lewisandclark/csd/basicfantasy/HomeActivity.java
@@ -78,20 +78,20 @@ public class HomeActivity extends AppCompatActivity {
     private ArrayList<Armor> buildArmorList() {
         ArrayList<Armor> armorList = new ArrayList<>();
 
-        armorList.add(new Armor(R.string.none,"",0,0,1,0,0,0,0));
-        armorList.add(new Armor(R.string.padded,"",0,0,1,0,0,0,0));
-        armorList.add(new Armor(R.string.hide,"",0,0,1,0,0,0,0));
-        armorList.add(new Armor(R.string.leather,"",0,0,1,0,0,0,0));
-        armorList.add(new Armor(R.string.studded,"",0,0,1,0,0,0,0));
-        armorList.add(new Armor(R.string.ring_mail,"",0,0,1,0,0,0,0));
-        armorList.add(new Armor(R.string.brigandine,"",0,0,1,0,0,0,0));
-        armorList.add(new Armor(R.string.chain_mail,"",0,0,1,0,0,0,0));
-        armorList.add(new Armor(R.string.scale,"",0,0,1,0,0,0,0));
-        armorList.add(new Armor(R.string.splint,"",0,0,1,0,0,0,0));
-        armorList.add(new Armor(R.string.banded,"",0,0,1,0,0,0,0));
-        armorList.add(new Armor(R.string.plate,"",0,0,1,0,0,0,0));
-        armorList.add(new Armor(R.string.field_plate,"",0,0,1,0,0,0,0));
-        armorList.add(new Armor(R.string.full_plate,"",0,0,1,0,0,0,0));
+        armorList.add(new Armor(R.string.none,"",0,0,1,11,11,0,0));
+        armorList.add(new Armor(R.string.padded,"",10,15,1,12,12,0,0));
+        armorList.add(new Armor(R.string.hide,"",30,10,1,13,13,0,0));
+        armorList.add(new Armor(R.string.leather,"",15,20,1,13,13,0,0));
+        armorList.add(new Armor(R.string.studded,"",25,30,1,14,14,0,0));
+        armorList.add(new Armor(R.string.ring_mail,"",30,25,1,14,14,0,0));
+        armorList.add(new Armor(R.string.brigandine,"",30,80,1,15,15,0,0));
+        armorList.add(new Armor(R.string.chain_mail,"",40,60,1,15,15,0,0));
+        armorList.add(new Armor(R.string.scale,"",55,80,1,16,16,0,0));
+        armorList.add(new Armor(R.string.splint,"",45,100,1,16,16,0,0));
+        armorList.add(new Armor(R.string.banded,"",35,200,1,16,16,0,0));
+        armorList.add(new Armor(R.string.plate,"",50,300,1,17,17,0,0));
+        armorList.add(new Armor(R.string.field_plate,"",70,500,1,18,18,0,0));
+        armorList.add(new Armor(R.string.full_plate,"",80,1500,1,19,19,0,0));
 
         return armorList;
     }
