0812cc7c4806bae6e01fe2664be8de6c268d3f6c
ThorinSchmidt <tschmidt@stcharlessd.org>
2018-12-07 12:17:09 -0600

    new file:   app/src/main/java/org/lewisandclark/csd/basicfantasy/Page2Activity.java

---
+++ app/src/main/java/org/lewisandclark/csd/basicfantasy/Page2Activity.java
@@ -0,0 +1,13 @@
+package org.lewisandclark.csd.basicfantasy;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Page2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_page1);
+    }
+}
