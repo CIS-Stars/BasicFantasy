a18d51200d62d1b5029b3bd27763d37a9120f909
Thorin Schmidt <tschmidt@stcharlessd.org>
2018-12-23 12:43:40 -0600

    update model layer

--- app/src/main/java/org/lewisandclark/csd/basicfantasy/HomeActivity.java
+++ app/src/main/java/org/lewisandclark/csd/basicfantasy/HomeActivity.java
@@ -10,15 +10,9 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import org.lewisandclark.csd.basicfantasy.model.Armor;
 import org.lewisandclark.csd.basicfantasy.model.CharacterList;
 import org.lewisandclark.csd.basicfantasy.model.EquipmentDatabase;
-import org.lewisandclark.csd.basicfantasy.model.Item;
 import org.lewisandclark.csd.basicfantasy.model.PlayerCharacter;
-import org.lewisandclark.csd.basicfantasy.model.Shield;
-import org.lewisandclark.csd.basicfantasy.model.Weapon;
-
-import java.util.ArrayList;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -48,9 +42,7 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        sArmors.addAll(buildArmorList());
-        sShields.addAll(buildShieldList());
-        sWeapons.addAll(buildWeaponList());
+
         Log.d("HOME","Character Array is size: " +
                 sCharacters.getList().size());
         sCurrentCharacterIndex =
@@ -61,7 +53,7 @@ public class HomeActivity extends AppCompatActivity {
             sCharacters.addCharacter(new PlayerCharacter());
             sCharacters.addCharacter(new PlayerCharacter("Marcia of Xant"));
             mCurrentCharacter = sCharacters.getPlayerCharacter(0);
-            mCurrentCharacter.addEquipment(sWeapons.);
+            //mCurrentCharacter.addEquipment(sWeapons.);
             sCharacters.updateCharacter(mCurrentCharacter,0);
         }
 
@@ -108,73 +100,5 @@ public class HomeActivity extends AppCompatActivity {
             Intent theIntent = BuyEquipmentActivity.newIntent(HomeActivity.this);
             startActivity(theIntent);
         });
-    }
-
-    private ArrayList<Shield> buildShieldList() {
-        ArrayList<Shield> list = new ArrayList<>();
-
-        list.add(new Shield(R.string.buckler, "", 2, 5, 1,
-                1, 0, 0, 0));
-        list.add(new Shield(R.string.medium, "", 5, 7, 1,
-                1, 1, 0, 0));
-        list.add(new Shield(R.string.tower, "", 12, 15, 1,
-                1, 3, 0, 0));
-
-        return list;
-    }
-
-    private ArrayList<Armor> buildArmorList() {
-        ArrayList<Armor> armorList = new ArrayList<>();
-
-        armorList.add(new Armor(R.string.padded,"",10,15,1,
-                12,12,0,0));
-        armorList.add(new Armor(R.string.hide,"",30,10,1,
-                13,13,0,0));
-        armorList.add(new Armor(R.string.leather,"",15,20,1,
-                13,13,0,0));
-        armorList.add(new Armor(R.string.studded,"",25,30,1,
-                14,14,0,0));
-        armorList.add(new Armor(R.string.ring_mail,"",30,25,1,
-                14,14,0,0));
-        armorList.add(new Armor(R.string.brigandine,"",30,80,1,
-                15,15,0,0));
-        armorList.add(new Armor(R.string.chain_mail,"",40,60,1,
-                15,15,0,0));
-        armorList.add(new Armor(R.string.scale,"",55,80,1,
-                16,16,0,0));
-        armorList.add(new Armor(R.string.splint,"",45,100,1,
-                16,16,0,0));
-        armorList.add(new Armor(R.string.banded,"",35,200,1,
-                16,16,0,0));
-        armorList.add(new Armor(R.string.plate,"",50,300,1,
-                17,17,0,0));
-        armorList.add(new Armor(R.string.field_plate,"",70,500,1,
-                18,18,0,0));
-        armorList.add(new Armor(R.string.full_plate,"",80,1500,1,
-                19,19,0,0));
-
-        return armorList;
-    }
-
-    private ArrayList<Weapon> buildWeaponList(){
-        ArrayList<Weapon> list = new ArrayList<>();
-
-        list.add(new Weapon(R.string.hand_axe, 4, 5, 6, 1, 0,
-                0, "", false, false, 0, 0,
-                0, ""));
-        list.add(new Weapon(R.string.battle_axe, 7, 7, 8, 1, 0,
-                0, "", false, false, 0, 0,
-                0, ""));
-        list.add(new Weapon(R.string.great_axe, 14, 15, 10, 1, 0,
-                0, "", true, false, 0, 0,
-                0, ""));
-        list.add(new Weapon(R.string.pickaxe, 6, 4, 6, 1, 0,
-                0, "", false, false, 0, 0,
-                0, ""));
-        list.add(new Weapon(R.string.mattock, 8, 6, 8, 1, 0,
-                0, "", false, false, 0, 0,
-                0, ""));
-
-        return list;
     }
 }
--- app/src/main/java/org/lewisandclark/csd/basicfantasy/model/EquipmentDatabase.java
+++ app/src/main/java/org/lewisandclark/csd/basicfantasy/model/EquipmentDatabase.java
@@ -2,6 +2,8 @@ package org.lewisandclark.csd.basicfantasy.model;
 
 import android.content.Context;
 
+import org.lewisandclark.csd.basicfantasy.R;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -27,5 +29,76 @@ public class EquipmentDatabase {
         mArmorList = new ArrayList<>();
         mWeaponList = new ArrayList<>();
         mShieldList = new ArrayList<>();
+        mArmorList.addAll(buildArmorList());
+        mShieldList.addAll(buildShieldList());
+        mWeaponList.addAll(buildWeaponList());
+    }
+
+    private ArrayList<Shield> buildShieldList() {
+        ArrayList<Shield> list = new ArrayList<>();
+
+        list.add(new Shield(R.string.buckler, "", 2, 5, 1,
+                1, 0, 0, 0));
+        list.add(new Shield(R.string.medium, "", 5, 7, 1,
+                1, 1, 0, 0));
+        list.add(new Shield(R.string.tower, "", 12, 15, 1,
+                1, 3, 0, 0));
+
+        return list;
+    }
+
+    private ArrayList<Armor> buildArmorList() {
+        ArrayList<Armor> armorList = new ArrayList<>();
+
+        armorList.add(new Armor(R.string.padded,"",10,15,1,
+                12,12,0,0));
+        armorList.add(new Armor(R.string.hide,"",30,10,1,
+                13,13,0,0));
+        armorList.add(new Armor(R.string.leather,"",15,20,1,
+                13,13,0,0));
+        armorList.add(new Armor(R.string.studded,"",25,30,1,
+                14,14,0,0));
+        armorList.add(new Armor(R.string.ring_mail,"",30,25,1,
+                14,14,0,0));
+        armorList.add(new Armor(R.string.brigandine,"",30,80,1,
+                15,15,0,0));
+        armorList.add(new Armor(R.string.chain_mail,"",40,60,1,
+                15,15,0,0));
+        armorList.add(new Armor(R.string.scale,"",55,80,1,
+                16,16,0,0));
+        armorList.add(new Armor(R.string.splint,"",45,100,1,
+                16,16,0,0));
+        armorList.add(new Armor(R.string.banded,"",35,200,1,
+                16,16,0,0));
+        armorList.add(new Armor(R.string.plate,"",50,300,1,
+                17,17,0,0));
+        armorList.add(new Armor(R.string.field_plate,"",70,500,1,
+                18,18,0,0));
+        armorList.add(new Armor(R.string.full_plate,"",80,1500,1,
+                19,19,0,0));
+
+        return armorList;
+    }
+
+    private ArrayList<Weapon> buildWeaponList(){
+        ArrayList<Weapon> list = new ArrayList<>();
+
+        list.add(new Weapon(R.string.hand_axe, 4, 5, 6, 1, 0,
+                0, "", false, false, 0, 0,
+                0, ""));
+        list.add(new Weapon(R.string.battle_axe, 7, 7, 8, 1, 0,
+                0, "", false, false, 0, 0,
+                0, ""));
+        list.add(new Weapon(R.string.great_axe, 14, 15, 10, 1, 0,
+                0, "", true, false, 0, 0,
+                0, ""));
+        list.add(new Weapon(R.string.pickaxe, 6, 4, 6, 1, 0,
+                0, "", false, false, 0, 0,
+                0, ""));
+        list.add(new Weapon(R.string.mattock, 8, 6, 8, 1, 0,
+                0, "", false, false, 0, 0,
+                0, ""));
+
+        return list;
     }
 }
